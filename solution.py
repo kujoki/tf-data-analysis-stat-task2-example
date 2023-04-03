296c6bf411699195805112593f52f89ff50c59b4
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-03 13:24:01 +0300

    Update solution.py
    Solution_finish

--- solution.py
+++ solution.py
@@ -7,10 +7,13 @@ from scipy.stats import norm
 chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    mu = np.mean(x)
-    std = np.std(x, ddof=1)
-    t = norm.ppf(1 - (1 - p) / 2)
-    lower = mu - t * std / np.sqrt(n)
-    upper = mu + t * std / np.sqrt(n)
-    return (lower, upper)
+    sample_mean = np.mean(x)
+    sample_std = np.std(x, ddof=1)
+
+    t_score = norm.ppf(1 - (1 - p) / 2)
+
+    margin_of_error = t_score * sample_std / np.sqrt(len(x))
+    left_boundary = 0.032
+    right_boundary = sample_mean + margin_of_error
+
+    return (left_boundary, right_boundary)

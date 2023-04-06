ad42f0cef47a6b145d8bc515ff30566f8542e0c1
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-06 12:48:44 +0300

    4-ая попытка.

--- solution.py
+++ solution.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import t
 
 from scipy.stats import norm
 
 
 chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
-  x_max = np.max(x)
+def solution(p, x):
+  alpha = 1 - p
   n = len(x)
-  g_1 = p ** (1/n)
-  g_2 = 1
-  left = (x_max - 0.032)/g_2 + 0.032
-  right = (x_max - 0.032)/g_1 + 0.032
-  return left, right
+  Xn = np.max(x)
+  variance = ((Xn - 0.032)**2) / 12 #дисперсия равномерного
+  t_val = t.ppf(1 - alpha/2, n-1)
+  lower = Xn - t_val * np.sqrt(variance / (n-1))
+  upper = Xn + t_val * np.sqrt(variance / (n-1))
+  return (lower, upper)
+

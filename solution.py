f1960bfc584d5388469c6d9d5712268292c9077f
Kate <90715013+kujoki@users.noreply.github.com>
2023-04-07 22:36:50 +0300

    Update solution.py
    Попытка построения точного доверительного интервала с помощью максимального значения выборки

--- solution.py
+++ solution.py
@@ -1,19 +1,16 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import t
-
 from scipy.stats import norm
 
 
 chat_id = 670223087 # Ваш chat ID, не меняйте название переменной
 
 def solution(p, x):
-  alpha = 1 - p
-  n = len(x)
-  Xn = np.max(x)
-  variance = ((Xn - 0.032)**2) / 12 #дисперсия равномерного
-  t_val = t.ppf(1 - alpha/2, n-1)
-  lower = Xn - t_val * np.sqrt(variance / (n-1))
-  upper = Xn + t_val * np.sqrt(variance / (n-1))
-  return (lower, upper)
+    alpha = 1 - p
+    n = len(x)
+    Xn = max(x)
+    upper = (Xn-0.032)/(alpha)**(1/n)+0.032
+    lower = Xn
+    return (lower, upper)
 
